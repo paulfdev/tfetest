31b56f510029042660496ff64c4cc65edeba6202
Paul Forsyth <paul.forsyth@ecs-digital.co.uk>
2018-10-09 10:44:04 +0100

    create aws key

---
+++ aws.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+variable "aws_access_key"{
+}
+
+variable "aws_secret_key" {
+
+}
+
+variable "aws_region" {
+
+}
